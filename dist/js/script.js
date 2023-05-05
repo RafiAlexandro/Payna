896114cf8ed341d7adbdcc3354830301a2367165
Rafi Alexandro <halloandro123@gmail.com>
2023-05-05 22:10:05 +0700

    update build

--- dist/js/script.js
+++ dist/js/script.js
@@ -37,13 +37,13 @@ $(document).ready(function () {
 
     for (var i = 0; i < list.length; i++) {
     list[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
+        var current = document.getElementsByClassName(" active");
 
         if (current.length > 0) {
-        current[0].className = current[0].className.replace("active", "");
+        current[0].className = current[0].className.replace(" active", "");
         }
 
-        this.className += "active";
+        this.className += " active";
     });
     }
 
